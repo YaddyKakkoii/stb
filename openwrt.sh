bf279be9d839725bbd7a9725da65d491ebdac46e
nonamez <127028273+YaddyKakkoii@users.noreply.github.com>
2024-07-19 23:09:09 +0700

    Update openwrt.sh

--- openwrt.sh
+++ openwrt.sh
@@ -1,9 +1,19 @@
 #!/data/data/com.termux/files/usr/bin/bash
+clear
 file_bin="/data/data/com.termux/files/usr/bin/stb"
 known_host="/data/data/com.termux/files/home/.ssh/known_hosts"
-if [[ -f ${file_bin} ]]; then
-    echo -e "sshpass -p "indonesia" ssh root@192.168.1.1" > ${file_bin}
-fi
+printf "\nSilakan masukkan password openwrt kamu\n default password adalah indonesia\n"
+read -p "MASUKKAN PASSWORD KAMU [ cth: indonesia ] : " passwordmu
 if [[ -f ${known_host} ]]; then
     rm -f ${known_host}
+fi
+if [[ ! -f ${file_bin} ]]; then
+    echo -e "sshpass -p "$passwordmu" ssh root@192.168.1.1" > ${file_bin}
+    chmod +x $PREFIX/bin/stb
+    ssh root@192.168.1.1 | echo -e "yes\n${passwordmu}\n${passwordmu}"
+else
+    rm -f ${file_bin}
+    echo -e "sshpass -p "$passwordmu" ssh root@192.168.1.1" > ${file_bin}
+    chmod +x $PREFIX/bin/stb
+    ssh root@192.168.1.1 | echo -e "yes\n${passwordmu}\n${passwordmu}"
 fi
