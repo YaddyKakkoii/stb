83383bdac1b1297480529e1b1d7038539c6baebc
nonamez <127028273+YaddyKakkoii@users.noreply.github.com>
2024-07-19 23:36:03 +0700

    Update termux_wrt.sh

--- termux_wrt.sh
+++ termux_wrt.sh
@@ -1,5 +1,8 @@
 #!/data/data/com.termux/files/usr/bin/bash
 clear
+function loginwrt(){
+ssh root@192.168.1.1
+}
 file_bin="/data/data/com.termux/files/usr/bin/stb"
 known_host="/data/data/com.termux/files/home/.ssh/known_hosts"
 printf "\nSilakan masukkan password openwrt kamu\n default password adalah indonesia\n"
@@ -7,13 +10,14 @@ read -p "MASUKKAN PASSWORD KAMU [ cth: indonesia ] : " passwordmu
 if [[ -f ${known_host} ]]; then
     rm -f ${known_host}
 fi
+echo -e "\n\nKetik:\nyes\n${passwordmu}\n${passwordmu}\n\nJika bengong tekan Ctrl d kemudian ketik stb lalu enter\n\n"
 if [[ ! -f ${file_bin} ]]; then
     echo -e "sshpass -p "$passwordmu" ssh root@192.168.1.1" > ${file_bin}
     chmod +x $PREFIX/bin/stb
-    ssh root@192.168.1.1 | echo -e "\n\nKetik:\nyes\n${passwordmu}\n${passwordmu}\n\nJika bengong tekan Ctrl d kemudian ketik stb lalu enter\n\n"
+    echo -e "yes\n${passwordmu}\n${passwordmu}" | loginwrt
 else
     rm -f ${file_bin}
     echo -e "sshpass -p "$passwordmu" ssh root@192.168.1.1" > ${file_bin}
     chmod +x $PREFIX/bin/stb
-    ssh root@192.168.1.1 | echo -e "\n\nKetik:\nyes\n${passwordmu}\n${passwordmu}\n\nJika bengong tekan Ctrl d kemudian ketik stb lalu enter\n\n"
+    echo -e "yes\n${passwordmu}\n${passwordmu}" | loginwrt
 fi
