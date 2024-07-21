3831feda1f7f8fac512bb07f216e1b561e0a2643
nonamez <127028273+YaddyKakkoii@users.noreply.github.com>
2024-07-21 08:23:03 +0700

    Create dnslib.sh

---
+++ dnslib.sh
@@ -0,0 +1,83 @@
+# BEGIN_VARIABLE
+DOMAIN=1xxx
+CF_KEY=2xxx
+CF_ID=3xxx
+# END_VARIABLE
+random=4xxx
+sub=5xxx
+b="\033[34;1m";m="\033[31;1m";h="\033[32;1m"
+p="\033[39;1m";u="\033[35;1m";c="\033[36;1m"
+k="\033[33;1m";n="\033[00m"
+function yaddykakkoii() {
+tyblue='\e[1;36m'
+NC='\e[0m'
+echo ""
+echo -e  "${tyblue}┌──────────────────────────────────────────┐${NC}"
+echo -e  "${tyblue}│              \033[1;37mTERIMA KASIH                ${tyblue}│${NC}"
+echo -e  "${tyblue}│         \033[1;37mSUDAH MENGGUNAKAN SCRIPT         ${tyblue}│${NC}"
+echo -e  "${tyblue}│    \033[1;37mDARI \033[33;1mYADDY KAKKOII GANTENG          ${tyblue}│${NC}"
+echo -e  "${tyblue}└──────────────────────────────────────────┘${NC}"
+echo ""
+}
+function cekdns(){
+#(</dev/urandom tr -dc a-z0-9 | head -c5)
+dns=${sub}.${DOMAIN}
+dns2=*.${dns}
+NS_DOMAIN=ns.${dns}
+set -euo pipefail
+IP="$1"
+ZONE=$(curl -sLX GET "https://api.cloudflare.com/client/v4/zones?name=${DOMAIN}&status=active" \
+-H "X-Auth-Email: ${CF_ID}" \
+-H "X-Auth-Key: ${CF_KEY}" \
+-H "Content-Type: application/json" | jq -r .result[0].id)
+RECORD=$(curl -sLX GET "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records?name=${dns}" \
+-H "X-Auth-Email: ${CF_ID}" \
+-H "X-Auth-Key: ${CF_KEY}" \
+-H "Content-Type: application/json" | jq -r .result[0].id)
+if [[ "${#RECORD}" -le 10 ]]; then
+RECORD=$(curl -sLX POST "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records" \
+-H "X-Auth-Email: ${CF_ID}" \
+-H "X-Auth-Key: ${CF_KEY}" \
+-H "Content-Type: application/json" \
+--data '{"type":"A","name":"'${dns}'","content":"'${IP}'","ttl":120,"proxied":false}' | jq -r .result.id)
+RECORD=$(curl -sLX POST "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records" \
+-H "X-Auth-Email: ${CF_ID}" \
+-H "X-Auth-Key: ${CF_KEY}" \
+-H "Content-Type: application/json" \
+--data '{"type":"A","name":"'${dns2}'","content":"'${IP}'","ttl":120,"proxied":false}' | jq -r .result.id)
+fi
+RESULT=$(curl -sLX PUT "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records/${RECORD}" \
+-H "X-Auth-Email: ${CF_ID}" \
+-H "X-Auth-Key: ${CF_KEY}" \
+-H "Content-Type: application/json" \
+--data '{"type":"A","name":"'${dns}'","content":"'${IP}'","ttl":120,"proxied":false}')
+RESULT=$(curl -sLX PUT "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records/${RECORD}" \
+-H "X-Auth-Email: ${CF_ID}" \
+-H "X-Auth-Key: ${CF_KEY}" \
+-H "Content-Type: application/json" \
+--data '{"type":"A","name":"'${dns2}'","content":"'${IP}'","ttl":120,"proxied":false}')
+RECORD=$(curl -sLX GET "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records?name=${NS_DOMAIN}" \
+-H "X-Auth-Email: ${CF_ID}" \
+-H "X-Auth-Key: ${CF_KEY}" \
+-H "Content-Type: application/json" | jq -r .result[0].id)
+if [[ "${#RECORD}" -le 10 ]]; then
+RECORD=$(curl -sLX POST "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records" \
+-H "X-Auth-Email: ${CF_ID}" \
+-H "X-Auth-Key: ${CF_KEY}" \
+-H "Content-Type: application/json" \
+--data '{"type":"NS","name":"'${NS_DOMAIN}'","content":"'${dns}'","ttl":120,"proxied":false}' | jq -r .result.id)
+fi
+RESULT=$(curl -sLX PUT "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records/${RECORD}" \
+-H "X-Auth-Email: ${CF_ID}" \
+-H "X-Auth-Key: ${CF_KEY}" \
+-H "Content-Type: application/json" \
+--data '{"type":"NS","name":"'${NS_DOMAIN}'","content":"'${dns}'","ttl":120,"proxied":false}')
+echo -e "\nDOMAIN KAMU : $dns"
+checkping="cekping.txt"
+printf "\n$c. cek ping $IP....\n"
+function cekping(){ ping -t 20 -c 1 $1; };
+cekping $IP | grep -w 'packets' | awk '{print $3}' > $checkping
+result=$(cat $checkping)
+}
+# Gunakan stream editor untuk menggunakan mini library ini
+# Yang Tau Tau aja Yang paham gunanya perpustakaan ini 😋 
